3c5d9780b01dca6455dc203df862378eddb01044
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-01-18 16:37:37 +0530

    Change the Test Component

--- test/test.js
+++ test/test.js
@@ -1,28 +1,36 @@
-import { findEmailByFullName, findEmailByName } from '../src/findmail.js';
+// import { findEmailByFullName, findEmailByName } from '../src/findmail.js';
 
-async function main() {
+// async function main() {
+//     try {
+//         const emailByFullName = await findEmailByFullName('microsoft.com', 'Satya Nadella');
+//         console.log('Result from Full Name:', emailByFullName);
+
+//         const emailByName = await findEmailByName('google.com', 'Sundar', 'Pichai');
+//         console.log('Result from Name:', emailByName);
+//     } catch (error) {
+//         console.error('Error:', error.message);
+//     }
+// }
+
+// main();
+
+
+import { verifyEmail } from '../src/verifymail.js';
+
+(async () => {
     try {
-        const emailByFullName = await findEmailByFullName('microsoft.com', 'Satya Nadella');
-        console.log('Result from Full Name:', emailByFullName);
-
-        const emailByName = await findEmailByName('google.com', 'Sundar', 'Pichai');
-        console.log('Result from Name:', emailByName);
+        const result = await verifyEmail("test@gmail.com");
+        console.log("Verification Result for test@gmail.com:", result);
+        // Expected Output: { isValid: true, category: "personal" }
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error("Error verifying email (test@gmail.com):", error.message);
     }
-}
 
-main();
-
-
-// import { verifyEmail } from '../src/verifymail.js';
-
-// (async () => {
-//   const result = await verifyEmail("test@gmail.com");
-//   console.log(result);
-//   // Output: { isValid: true, category: "personal" }
-
-//   const disposableResult = await verifyEmail("fake@tempmail.com");
-//   console.log(disposableResult);
-//   // Output: { isValid: false, category: "disposable", reason: "Disposable email addresses are not allowed" }
-// })();
+    try {
+        const disposableResult = await verifyEmail("fake@tempmail.com");
+        console.log("Verification Result for fake@tempmail.com:", disposableResult);
+        // Expected Output: { isValid: false, category: "disposable", reason: "Disposable email addresses are not allowed" }
+    } catch (error) {
+        console.error("Error verifying email (fake@tempmail.com):", error.message);
+    }
+})();
